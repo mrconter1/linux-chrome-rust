ed1e5dedee2da08364fd1f5e119b8337a4d5d360
Rasmus Lindahl <mrconter1@gmail.com>
2021-01-06 23:35:56 +0100

    Update run-chrome.rs

--- run-chrome.rs
+++ run-chrome.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     Command::new("google-chrome")
         .spawn()
-        .expect("ls command failed to start");
+        .expect("Oh, boy. Either Chrome isn't installed or reality doesn't exist.");
 }
 
                                                                
